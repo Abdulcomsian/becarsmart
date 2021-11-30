2f6ceb4f4100a48bf4597d1c4e463910dcecbacc
aaqibnawaz <you@example.com>
2021-11-30 12:34:21 +0500

    login changes

--- resources/views/layouts/dashboard/side-bar.blade.php
+++ resources/views/layouts/dashboard/side-bar.blade.php
@@ -81,7 +81,7 @@
 									</span>
                     <div class="menu-sub menu-sub-accordion">
                         <div class="menu-item">
-                            <a class="menu-link" href="{{ url('/buy-car') }}">
+                            <a class="menu-link" href="{{ url('/home') }}">
 												<span class="menu-bullet">
 													<span class="bullet bullet-dot"></span>
 												</span>
