47b0d0e641ffa4f3de1d363bab919764bec19777
obaidjani <muhammad.obaid@dazaran.com>
2021-12-23 17:56:19 +0500

    some changes done

---
+++ database/migrations/2021_12_23_122212_create_sell_car_lead_images_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSellCarLeadImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sell_car_lead_images', function (Blueprint $table) {
+            $table->id();
+            $table->string('image_name')->nullable();
+             $table->bigInteger('sell_car_lead_id')->unsigned();
+            $table->foreign('sell_car_lead_id')->references('id')->on('sell_car_leads')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sell_car_lead_images');
+    }
+}
