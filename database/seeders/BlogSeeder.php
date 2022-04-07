e2953ab561ad402209a8c0ae27f5bdf4b4802095
atif-masood <rajaatif927@gmail.com>
2022-04-07 11:35:49 +0500

    done all changes 7

--- database/seeders/BlogSeeder.php
+++ database/seeders/BlogSeeder.php
@@ -31,7 +31,7 @@ class BlogSeeder extends Seeder
                 'exceed' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
                 'feature_img' => 'featuredImage.png',
                 'message' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
-                'permalink' => 'How to sell on BeCarSmart',
+                'permalink' => 'How to use BeCarSmart',
                 'user_id' => 1,
                 'created_at' => date("Y-m-d H:i:s"),
                 'updated_at' => date("Y-m-d H:i:s"),
