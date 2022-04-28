33b48d9dcabf9a05233e8c5e4f840d82c3fc145f
atif-masood <rajaatif927@gmail.com>
2022-04-28 23:49:35 +0500

    landing form issues 14

--- resources/views/frontend/mails/email_template1.blade.php
+++ resources/views/frontend/mails/email_template1.blade.php
@@ -4,6 +4,10 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title>Email Template 1</title>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
     <style>
 
     </style>
@@ -40,12 +44,16 @@
         <br><br> The payment will be made on collection. It will be an instant <br> bank transfer payment.</p>
         <br><br>
             
-            <div style="background:#2c3b53; padding:8px; ">
+            <div class="footer-container" style="background:#2c3b53; padding:8px; ">
                 <p style="font-size:15px; text-align:center; color:#fff; font-weight:600;">Why Choose Us?</p>
-                <div>
-                    <p style="text-align: center; color:#fff; font-weight:600; font-size:12px;">1.  Free collection</p>
-                    <p style="text-align: center; color:#fff; font-weight:600; font-size:12px;">2.  Honest - Haggle free service</p>
-                    <p style="text-align: center; color:#fff; font-weight:600; font-size:12px;">3.  Instant payment</p>
+                <div class="row">
+                    <div class="col-5"></div>
+                    <div class="choose-us col-3" style="padding-left:20px;">
+                        <p style=" color:#fff; font-weight:600; font-size:12px;">1.  Free collection</p>
+                        <p style=" color:#fff; font-weight:600; font-size:12px;">2.  Honest - Haggle free service</p>
+                        <p style=" color:#fff; font-weight:600; font-size:12px;">3.  Instant payment</p>
+                    </div>
+                    <div class="col-4"></div>
                 </div>
                 
                 
