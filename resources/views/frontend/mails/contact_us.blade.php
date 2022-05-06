dfc24a821dffcae7a77b1ee7a2fdab16c995fa6e
atif-masood <rajaatif927@gmail.com>
2022-05-06 12:30:39 +0500

    email of conatct us 14

---
+++ resources/views/frontend/mails/contact_us.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Email Template 1</title>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+    <style>
+
+    </style>
+</head>
+
+<body style=" margin:0px; font-family: 'Poppins', sans-serif;">
+    <section id="header" style="width: 100%; max-width:80%;  margin:0 auto;">
+        <div class="header-content" style="text-align: center;">
+            <img src="{{asset ('assets/img/fff.svg')}}" width="45%" />
+        </div>
+        <p style="color: #2c3b53;font-size: 32px; font-weight: 700; text-align:center">Thank you for  <br> contacting us!</p>
+        
+        <br><br>
+        <p style="color: #747474;font-size: 16px; font-weight: 500; text-align:center">This automatic reply is just to let you know that we received your message and we’ll get back to you with a response as quickly as possible.
+        <br><br>If you have any additional information that you think will help us to assist you, please feel free to reply to this email.
+        
+        <br> We look forward to chatting soon!
+        <br><br>
+        
+        Thanks
+        
+        <div class="footer-container" style="background:#2c3b53; padding:8px; ">
+                <p style="font-size:15px; text-align:center; color:#fff; font-weight:600;">Why Choose Us?</p>
+                <div class="row">
+                    <div class="col-4"></div>
+                    <div class="choose-us col-4" style="padding-left:20px;">
+                        <p style=" color:#fff; font-weight:600; font-size:12px;">1.  Free collection</p>
+                        <p style=" color:#fff; font-weight:600; font-size:12px;">2.  Honest - Haggle free service</p>
+                        <p style=" color:#fff; font-weight:600; font-size:12px;">3.  Instant payment</p>
+                    </div>
+                    <div class="col-4"></div>
+                </div>
+                
+                
+            </div>
+            
+
+
+    </section>
+</body>
+</html>
