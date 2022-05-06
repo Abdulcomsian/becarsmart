70bac9ab9029f119de9f945db9d7e409a3f15049
atif-masood <rajaatif927@gmail.com>
2022-05-06 11:50:41 +0500

    email of conatct us 10

--- app/Http/Controllers/Frontend/ContactController.php
+++ app/Http/Controllers/Frontend/ContactController.php
@@ -18,12 +18,7 @@ class ContactController extends Controller
     public function store(Request $request)
     {
         $input = $request->except('_token');
-        Mail::send(array(), array(), function ($message) use ($input) {
-            $message->to('info@becarsmart.co.uk')
-                ->subject('contact us mail')
-                ->from($input['email'])
-                ->setBody($input['message'], 'text/html');
-        });
+        Notification::route('mail', $request->email)->notify(new TradersNotification($request->all()));
 
         
         toastSuccess('Your query send to admin successfully');
