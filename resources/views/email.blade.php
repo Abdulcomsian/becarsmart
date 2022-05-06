5ea5154db8c8cb3f5a3f0989e05d73518c3aaaaa
atif-masood <rajaatif927@gmail.com>
2022-05-06 11:29:16 +0500

    email of conatct us 8

---
+++ resources/views/email.blade.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Email Template 1</title>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+    <style>
+
+    </style>
+</head>
+
+<body style=" margin:0px; font-family: 'Poppins', sans-serif;">
+    <section id="header" style="width: 100%; max-width:80%;  margin:0 auto;">
+        <div class="header-content" style="text-align: center;">
+            <img src="{{asset ('assets/img/fff.svg')}}" width="45%" />
+        </div>
+        <p style="color: #2c3b53;font-size: 32px; font-weight: 700; text-align:center">Thank you for  <br> completing our vehicle <br> valuation form.</p>
+        <br>
+        <div class="multipale-img" style="text-align: center; width:100%; display:inline-flex;max-width: 100%;">
+            <div style="width:25%">
+                <img src="{{asset ('assets/img/enterreg.png')}}" style="width:50%;" />
+            </div>
+            <div style="width:25%">
+                <img src="{{asset ('assets/img/microsoft2.png')}}" style="width:50%;" />
+            </div>
+            <div style="width:25%">
+                <img src="{{asset ('assets/img/DEAL.png')}}" style="width:50%;" />
+            </div>
+            <div style="width:25%">
+                <img src="{{asset ('assets/img/microsoft2.png')}}" style="width:50%;" />
+            </div>
+        </div>
+        <br><br>
+        <p style="color: #747474;font-size: 16px; font-weight: 500; text-align:center">You are now one step closer to selling your vehicle.
+        <br><br> What happens next? One of our agents will be in contact. They
+        <br> will confirm all the vehicle details are accurate and give you a
+        <br> guaranteed price.
+        <br><br> Once you are happy with the offer and wish to accept it, we can
+        <br> move forward and arrange a suitable collection date.
+        <br><br> The payment will be made on collection. It will be an instant <br> bank transfer payment.</p>
+        <br><br>
+            
+            <div class="footer-container" style="background:#2c3b53; padding:8px; ">
+                <p style="font-size:15px; text-align:center; color:#fff; font-weight:600;">Why Choose Us?</p>
+                <div class="row">
+                    <div class="col-4"></div>
+                    <div class="choose-us col-4" style="padding-left:20px;">
+                        <p style=" color:#fff; font-weight:600; font-size:12px;">1.  Free collection</p>
+                        <p style=" color:#fff; font-weight:600; font-size:12px;">2.  Honest - Haggle free service</p>
+                        <p style=" color:#fff; font-weight:600; font-size:12px;">3.  Instant payment</p>
+                    </div>
+                    <div class="col-4"></div>
+                </div>
+                
+                
+            </div>
+
+
+    </section>
+</body>
+</html>
